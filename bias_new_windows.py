2e422c2368f48f84dcf23cbdc08b7ee8408d4f8b
Sophia Rubens <116817240+sophiarubens@users.noreply.github.com>
2025-03-12 10:14:27 -0400

    separating Re/Im so quadpack wrappers can handle

--- bias_new_windows.py
+++ bias_new_windows.py
@@ -6,6 +6,10 @@ from scipy.integrate import quad,dblquad
 import camb
 from camb import model
 
+infty=np.infty
+pi=np.pi
+twopi=2.*pi
+
 scale=1e-9
 def get_mps(pars,zs,minkh=1e-4,maxkh=1,npts=200): # < CAMBpartial < buildCAMBpartials
     '''
@@ -93,7 +97,7 @@ def cornerplot(fishermat,params,pnames,nsamp=10000,savename=None):
 
 def Wmat(kvec,sigma): # USE THIS AS A TEMPLATE FOR THE MORE-COMPLICATED NUMERICAL VERSION
    k,kp=np.meshgrid(kvec,kvec)
-   return 2*np.pi*sigma**2*np.exp(-sigma**2*(k-kp)**2)
+   return twopi*sigma**2*np.exp(-sigma**2*(k-kp)**2)
 
 def bias(F,B):
     return np.linalg.inv(F)@B
@@ -103,22 +107,105 @@ def printparswbiases(pars,parnames,biases):
         print('{:12} = {:-10.3e} with bias {:-12.5e}'.format(parnames[p], par, biases[p]))
     return None
 
-# quad(   func, a, b,             args=(), full_output=0, epsabs=1.49e-08, epsrel=1.49e-08, limit=50, points=None, weight=None, wvar=None, wopts=None, maxp1=50, limlst=50, complex_func=False)
+# quad(   func, a, b,             args=(), full_output=0, epsabs=1.49e-08, epsrel=1.49e-08, limit=50, points=None, weight=None, wvar=None, wopts=None, maxp1=50, limlst=50, complex_func=False) # ** "If func takes many arguments, it is integrated along the axis corresponding to the first argument."
 # dblquad(func, a, b, gfun, hfun, args=(), epsabs=1.49e-08, epsrel=1.49e-08)
 # nquad(  func, ranges, args=None, opts=None, full_output=False)
-theta_like=nquad() # will need to modularize the parts inside and outside the mod-squared
-phi_like=nquad()
-def W_binned_airy_beam(rk,rkp,thetalike,philike):
-    r_like=nquad()
-    return 4*np.pi**2*r_like*theta_like*phi_like
 
-# # Example usage:
-# r1 = np.array([1, 0, 0])
-# r2 = np.array([0, 1, 0])
-# result = integrate_spherical(r1, r2)
-# print(result)
+# argstuple, optional
+# Extra arguments to pass to func.
 
+# if you have a function fcn(var,par0,par1,...,parnm1), you need to specify the values of par0 through parnm1 using args as:
+# args=(par0val,par1val,...,parnm1val,)
 
+def r_arg_real(r,rk,rkp,sig):
+    arg=np.exp(-1j*(rk-rkp)*r-((r**2)/(2*sig**2)))*r**2
+    return arg.real
+def r_arg_imag(r,rk,rkp,sig):
+    arg=np.exp(-1j*(rk-rkp)*r-((r**2)/(2*sig**2)))*r**2
+    return arg.imag
+def inner_r_integral_real(rk,rkp,sig):
+    integral,_=quad(r_arg_real,0,np.infty,args=(rk,rkp,sig,))
+    return integral
+def inner_r_integral_imag(rk,rkp,sig):
+    integral,_=quad(r_arg_imag,0,np.infty,args=(rk,rkp,sig,))
+    return integral
+
+def theta_arg_real(theta,thetak,thetakp):
+    arg=np.exp(-1j*(thetak-thetakp)*theta)*(j1(theta)/theta)**2*np.sin(theta)
+    return arg.real
+def theta_arg_imag(theta,thetak,thetakp):
+    arg=np.exp(-1j*(thetak-thetakp)*theta)*(j1(theta)/theta)**2*np.sin(theta)
+    return arg.imag
+def inner_theta_integral_real(thetak,thetakp):
+    integral,_=quad(theta_arg_real,0,np.pi,args=(thetak,thetakp,))
+    return integral
+def inner_theta_integral_imag(thetak,thetakp):
+    integral,_=quad(theta_arg_imag,0,np.pi,args=(thetak,thetakp,))
+    return integral
+def theta_k_and_kp_arg(thetak,thetakp):
+    inner_theta_integral=inner_theta_integral_real(thetak,thetakp)**2+inner_theta_integral_imag(thetak,thetakp)**2
+    return inner_theta_integral*np.sin(thetak)*np.sin(thetakp)
+
+def phi_arg_real(phi,phik,phikp):
+    arg=np.exp(-1j*(phik-phikp)*phi)
+    return arg.real
+def phi_arg_imag(phi,phik,phikp):
+    arg=np.exp(-1j*(phik-phikp)*phi)
+    return arg.imag
+def inner_phi_integral_real(phik,phikp):
+    integral,_=quad(phi_arg_real,0,2*np.pi,args=(phik,phikp,))
+    return integral
+def inner_phi_integral_imag(phik,phikp):
+    integral,_=quad(phi_arg_imag,0,2*np.pi,args=(phik,phikp,))
+    return integral
+def phi_k_and_kp_arg(phik,phikp):
+    return inner_phi_integral_real(phik,phikp)**2+inner_phi_integral_imag(phik,phikp)**2
+
+def W_binned_airy_beam(rk,rkp,sig):
+    r_like=inner_r_integral_real(rk,rkp,sig)**2+inner_r_integral_imag(rk,rkp,sig)**2
+    theta_like=dblquad(theta_k_and_kp_arg,0,np.pi,0,np.pi) # thetak and thetakp aren't args; they're variables of integration
+    phi_like=dblquad(phi_k_and_kp_arg,0,2*np.pi,0,2*np.pi) # phik and phikp are vars of integration
+    print('in W_binned_airy_beam: done calculating r_like')
+    return 4*pi**2*r_like*theta_like*phi_like 
+
+# ######### OLD VERSION WITH NOT ENOUGH REAL/IMAG SEPARATION FROM TUES MARCH 11TH
+# def theta_arg(theta,thetak,thetakp):
+#     return np.exp(-1j*(thetak-thetakp)*theta)*(j1(theta)/theta)**2*np.sin(theta)
+# def theta_k_and_kp_arg(thetak,thetakp):
+#     inner_theta_integral,_=quad(theta_arg,0,pi,args=(thetak,thetakp,))
+#     return np.abs(inner_theta_integral)**2*np.sin(thetak)*np.sin(thetakp)
+# theta_like=dblquad(theta_k_and_kp_arg,0,pi,0,pi)
+# print('in main: done calculating theta_like')
+
+# def phi_arg(phi,phik,phikp):
+#     return np.exp(-1j*(phik-phikp)*phi)
+# def phi_k_and_kp_arg(phik,phikp):
+#     inner_phi_integral,_=quad(phi_arg,0,twopi,args=(phik,phikp,))
+#     return np.abs(inner_phi_integral)**2
+# phi_like=dblquad(phi_k_and_kp_arg,0,twopi,0,twopi)
+# print('in main: done calculating phi_like')
+
+# def W_binned_airy_beam(rk,rkp,sig):
+#     inner_r_integral,_=quad(r_arg,0,np.infty,args=(rk,rkp,sig,))
+#     r_like=inner_r_integral**2
+#     print('in W_binned_airy_beam: done calculating r_like')
+#     return 4*pi**2*r_like*theta_like*phi_like # boo global variables or whatever. but alas. I am a physicist at heart. 
+# #########
+
+npts=799
+rkmax=100.
+rk_test=np.linspace(0,rkmax,npts)
+rkp_test=np.copy(rk_test)
+sig_test=rkmax/2
+W_binned_airy_beam_test=W_binned_airy_beam(rk_test,rkp_test,sig_test)
+
+plt.figure()
+plt.imshow(W_binned_airy_beam_test)
+plt.colorbar()
+plt.xlabel('rk [dimensions of 1/L; units not specified]')
+plt.ylabel('rkp [dimensions of 1/L; units not specified]')
+plt.title('Visualization check: spherical harmonic binning of an Airy beam')
+plt.show()
 
 # # reality check ... does this seem like the Bessel function I meant to use and the Airy beam I meant to describe?
 # lim=25
