6aa4171856a76680fa335557c2160e8339b6c42c
Sophia Rubens <116817240+sophiarubens@users.noreply.github.com>
2025-03-12 14:17:49 -0400

    lots of stats for the complex_func=True structure
    By now, it's looking like the complex_func=True strategy is slower, but I need to prep the correct metadata in my driver function so I can drop in the Re/Im separated version in for a better side-by-side

--- bias_new_windows.py
+++ bias_new_windows.py
@@ -110,97 +110,70 @@ def printparswbiases(pars,parnames,biases):
 
 # NUMERICALLY INTEGRATING TO OBTAIN A SPHERICALLY HARMONICALLY BINNED WINDOW FUNCTION
 # THIS USES AN AIRY BEAM WITH GAUSSIAN CHROMATICITY ... WORK THROUGH THE BUGS WITH THIS, BUT GENERALIZE LATER
-def r_arg_real(r,rk,rkp,sig):
-    arg=np.exp(-1j*(rk-rkp)*r-((r**2)/(2*sig**2)))*r**2
-    return arg.real
-def r_arg_imag(r,rk,rkp,sig):
-    arg=np.exp(-1j*(rk-rkp)*r-((r**2)/(2*sig**2)))*r**2
-    return arg.imag
-def inner_r_integral_real(rk,rkp,sig):
-    integral,_=quad(r_arg_real,0,np.infty,args=(rk,rkp,sig,))
-    return integral
-def inner_r_integral_imag(rk,rkp,sig):
-    integral,_=quad(r_arg_imag,0,np.infty,args=(rk,rkp,sig,))
+def r_arg(r,rk,rkp,sig):
+    return np.exp(-1j*(rk-rkp)*r-((r**2)/(2*sig**2)))*r**2
+def inner_r_integral(rk,rkp,sig):
+    integral,_=quad(r_arg,0,np.infty,args=(rk,rkp,sig,),complex_func=True)
     return integral
 
-def theta_arg_real(theta,thetak,thetakp):
-    arg=np.exp(-1j*(thetak-thetakp)*theta)*(j1(theta)/theta)**2*np.sin(theta)
-    return arg.real
-def theta_arg_imag(theta,thetak,thetakp):
-    arg=np.exp(-1j*(thetak-thetakp)*theta)*(j1(theta)/theta)**2*np.sin(theta)
-    return arg.imag
-def inner_theta_integral_real(thetak,thetakp):
-    integral,_=quad(theta_arg_real,0,pi,args=(thetak,thetakp,))
-    return integral
-def inner_theta_integral_imag(thetak,thetakp):
-    integral,_=quad(theta_arg_imag,0,pi,args=(thetak,thetakp,))
+def theta_arg(theta,thetak,thetakp):
+    return np.exp(-1j*(thetak-thetakp)*theta)*(j1(theta)/theta)**2*np.sin(theta)
+def inner_theta_integral(thetak,thetakp):
+    integral,_=quad(theta_arg,0,pi,args=(thetak,thetakp,),complex_func=True)
     return integral
 def theta_k_and_kp_arg(thetak,thetakp):
-    inner_theta_integral=inner_theta_integral_real(thetak,thetakp)**2+inner_theta_integral_imag(thetak,thetakp)**2
-    return inner_theta_integral*np.sin(thetak)*np.sin(thetakp)
+    return np.abs(inner_theta_integral(thetak,thetakp))**2*np.sin(thetak)*np.sin(thetakp) # apparently it makes all the difference to do np.abs()**2 instead of manually taking ()**2
 
-def phi_arg_real(phi,phik,phikp):
-    arg=np.exp(-1j*(phik-phikp)*phi)
-    return arg.real
-def phi_arg_imag(phi,phik,phikp):
-    arg=np.exp(-1j*(phik-phikp)*phi)
-    return arg.imag
-def inner_phi_integral_real(phik,phikp):
-    integral,_=quad(phi_arg_real,0,twopi,args=(phik,phikp,))
-    return integral
-def inner_phi_integral_imag(phik,phikp):
-    integral,_=quad(phi_arg_imag,0,twopi,args=(phik,phikp,))
+def phi_arg(phi,phik,phikp):
+    return np.exp(-1j*(phik-phikp)*phi)
+def inner_phi_integral(phik,phikp):
+    integral,_=quad(phi_arg,0,twopi,args=(phik,phikp,),complex_func=True)
     return integral
 def phi_k_and_kp_arg(phik,phikp):
-    return inner_phi_integral_real(phik,phikp)**2+inner_phi_integral_imag(phik,phikp)**2
+    return np.abs(inner_phi_integral(phik,phikp))**2
 
 def W_binned_airy_beam_entry(rk,rkp,sig): # ONE ENTRY in the kind of W_binned square array that is useful to build
-    r_like=inner_r_integral_real(rk,rkp,sig)**2+inner_r_integral_imag(rk,rkp,sig)**2
-    # print('r_like=',r_like)
+    r_like=np.abs(inner_r_integral(rk,rkp,sig))**2
+    print('r_like=',r_like)
     theta_like,_=dblquad(theta_k_and_kp_arg,0,pi,0,pi) # thetak and thetakp aren't args; they're variables of integration
-    # print('theta_like=',theta_like)
+    print('theta_like=',theta_like)
     phi_like,_=dblquad(phi_k_and_kp_arg,0,twopi,0,twopi) # phik and phikp are vars of integration
-    # print('phi_like=',phi_like)
+    print('phi_like=',phi_like)
     return 4*pi**2*r_like*theta_like*phi_like 
 
-def W_binned_airy_beam(rk_vector,sig,save=True,timeout=600,verbose=False): # accumulate the kind of term we're interested in into a square grid
+def W_binned_airy_beam(rk_vector,sig,save=True,timeout=200,verbose=False): # accumulate the kind of term we're interested in into a square grid
+    earlyexit=False # so far
     t0=time.time()
     npts=len(rk_vector)
     arr=np.zeros((npts,npts))
+    element_times=np.zeros(npts**2)
     for i in range(npts):
         for j in range(i,npts):
             t1=time.time()
             arr[i,j]=W_binned_airy_beam_entry(rk_vector[i],rk_vector[j],sig)
             arr[j,i]=arr[i,j] # probably a negligible difference to leave it this way vs. adding an if stmt to manually catch the off-diagonal terms
             t2=time.time()
+            element_times[i*npts+j]=t2-t1
             if verbose:
                 print('populated entries [',i,',',j,'] and [',j,',',i,'] of W_binned_airy_beam in',t2-t1,'s')
             if(t2-t0>timeout):
+                earlyexit=True
                 break
     if (save):
         np.save('W_binned_airy_beam_array_'+str(time.time())+'.txt',arr)
     t3=time.time()
     if verbose:
-        print('filled the',npts,'x',npts,'W_binned_airy_beam_array in',t3-t0,'s')
+        if earlyexit:
+            print('due to time constraints, off-diagonal entries with row AND column indices above',i-1,': were not populated')
+        print('evaluation took',t3-t0,'s')
+        print('eval time per element:',np.mean(element_times),'+/-',np.std(element_times))
     return arr
 
-npts=20
+npts=12
 rkmax=100.
 rk_test=np.linspace(0,rkmax,npts)
 sig_test=rkmax/2
 W_binned_airy_beam_array_test=W_binned_airy_beam(rk_test,sig_test,verbose=True)
-# W_binned_airy_beam_array_test=np.zeros((npts,npts))
-# t00=time.time()
-# for i in range(npts):
-#     for j in range(npts):
-#         t0=time.time()
-#         W_binned_airy_beam_array_test[i,j]=W_binned_airy_beam(rk_test[i],rk_test[j],sig_test)
-#         t1=time.time()
-#         print('populated W_binned_airy_beam_array_test[',i,',',j,'] in',t1-t0,'s')
-#         if (t1-t00>600.):
-#             break # just to figure out if weird things are actually happening in the integral or my previous weird results were because my k-grid was too low in resolution
-#         # assert(1==0), "still trying to debug a single point"
-# np.save('W_binned_airy_beam_test',W_binned_airy_beam_array_test)
 
 plt.figure()
 plt.imshow(W_binned_airy_beam_array_test,extent=[rk_test[0],rk_test[-1],rk_test[-1],rk_test[0]]) # L,R,B,T
