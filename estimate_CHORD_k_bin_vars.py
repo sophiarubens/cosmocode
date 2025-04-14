fe61ef855c1b41261e5355ff5afa9895559f1b8a
Sophia Rubens <116817240+sophiarubens@users.noreply.github.com>
2025-04-14 11:02:15 -0400

    back to basics

--- estimate_CHORD_k_bin_vars.py
+++ estimate_CHORD_k_bin_vars.py
@@ -24,28 +24,29 @@ chord_x,chord_y=np.genfromtxt(pstart+path_to_chord_ant_pos_211208).T
 nants=len(chord_x)
 print("nants check:",nants)
 CHORD_appx_unif_elev=546.5
-chord_z_appx_old=CHORD_appx_unif_elev*np.ones(nants)
+# chord_z_appx_old=CHORD_appx_unif_elev*np.ones(nants)
+chord_z_appx=CHORD_appx_unif_elev*np.ones(nants)
 
-chord_z_row=np.linspace(CHORD_appx_unif_elev+1,CHORD_appx_unif_elev,24) # still pretty hacky ... my function makes a square, but I need a rectangle, so treat the row as the one with the larger dimension and truncate the rectangle after the fact
-chord_z_appx=diag_stripe_arr(chord_z_row)[:,:-2] # the return is a rectangle, but I need a vector-like thing ... reshape and check the plot to see if the x- and y- dims are ordered as expected in the telecon file
-plt.figure()
-plt.imshow(chord_z_appx)
-plt.colorbar()
-plt.show()
-# assert(1==0),"debugging z improvement"
-chord_z_appx=chord_z_appx.flatten()
-print("chord_x.shape=",chord_x.shape)
-print("chord_y.shape=",chord_y.shape)
-print("chord_z_appx.shape=",chord_z_appx.shape)
+# chord_z_row=np.linspace(CHORD_appx_unif_elev+1,CHORD_appx_unif_elev,24) # still pretty hacky ... my function makes a square, but I need a rectangle, so treat the row as the one with the larger dimension and truncate the rectangle after the fact
+# chord_z_appx=diag_stripe_arr(chord_z_row)[:,:-2] # the return is a rectangle, but I need a vector-like thing ... reshape and check the plot to see if the x- and y- dims are ordered as expected in the telecon file
+# plt.figure()
+# plt.imshow(chord_z_appx)
+# plt.colorbar()
+# plt.show()
+# # assert(1==0),"debugging z improvement"
+# chord_z_appx=chord_z_appx.flatten()
+# print("chord_x.shape=",chord_x.shape)
+# print("chord_y.shape=",chord_y.shape)
+# print("chord_z_appx.shape=",chord_z_appx.shape)
 
-plt.figure()
-p=plt.scatter(chord_x,chord_y,chord_z_appx)
-p.set_sizes([0.5])
-plt.xlabel("northing (m)")
-plt.ylabel("easting (m)")
-plt.title("Oversimplified CHORD")
-plt.colorbar()
-plt.show()
+# plt.figure()
+# p=plt.scatter(chord_x,chord_y,chord_z_appx)
+# p.set_sizes([0.5])
+# plt.xlabel("northing (m)")
+# plt.ylabel("easting (m)")
+# plt.title("Oversimplified CHORD")
+# plt.colorbar()
+# plt.show()
 
 chord_appx_antpos=np.vstack([chord_x,chord_y,chord_z_appx]).T*un.m
 print("chord_appx_antpos.shape=",chord_appx_antpos.shape)
@@ -67,12 +68,8 @@ hera_sensitivity = PowerSpectrum(
     )
 )
 
-# k_21_900MHz=np.load(pstart+path_to_working_dir+"/camb_k.npy")
-# P_21_900MHz=np.load(pstart+path_to_working_dir+"/camb_P.npy")
-
 def delta_from_P(k,P):
     return k**3*P/(2.*np.pi**2)
-# delta2_21_900MHz=delta_from_P(k_21_900MHz,P_21_900MHz)
 
 chord_sensitivity = PowerSpectrum(
     observation=Observation( # higher-order correction: 21cmSense defaults to Planck15, but elsewhere I'm using Planck18 (would be good to eventually remove the discrepancy)
@@ -100,30 +97,3 @@ print("np.array(chord_21cmse_k1d)=",np.array(chord_21cmse_k1d))
 
 np.save("chord_21cmse_k1d.npy",np.array(chord_21cmse_k1d))
 np.save("chord_21cmse_sigk.npy",np.array(chord_21cmse_sigk))
-
-# 49.320902, -119.621864 # middle-ish of the CHORD site, according to the pin I dropped using the satellite view of Google Maps
-# 49.321,    -119.622    # still in the middle-ish of the CHORD site
-# ns_step=8.5 # all spatial values here are in m
-# ew_step=6.3
-# northing_start=5.466e6
-# easting_start=309420
-# n_dishes_ns=24
-# n_dishes_ew=22
-# CHORD_appx_unif_elev=546.5
-# CHORD_ish_x=np.arange(easting_start, easting_start+ n_dishes_ew*ew_step,ew_step)
-# CHORD_ish_y=np.arange(northing_start,northing_start+n_dishes_ns*ns_step,ns_step)
-# CHORD_ish_X,CHORD_ish_Y=np.meshgrid(CHORD_ish_x,CHORD_ish_y)
-# print("CHORD_ish_X.shape=",CHORD_ish_X.shape)
-# print("CHORD_ish_Y.shape=",CHORD_ish_Y.shape)
-# CHORD_ish_Z=CHORD_appx_unif_elev*np.ones((n_dishes_ns,n_dishes_ew))
-# print("CHORD_ish_Z.shape=",CHORD_ish_Z.shape)
-# CHORD_ish_dishes=np.dstack([CHORD_ish_X,CHORD_ish_Y,CHORD_ish_Z])
-
-# plt.figure()
-# p=plt.scatter(CHORD_ish_X,CHORD_ish_Y,CHORD_ish_Z)
-# p.set_sizes([0.5])
-# plt.xlabel("northing (m)")
-# plt.ylabel("easting (m)")
-# plt.title("Oversimplified CHORD")
-# plt.colorbar()
-# plt.show()
