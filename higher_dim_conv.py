5c3cc5a9e5721600ada0390b43f57600cca004c3
Sophia Rubens <116817240+sophiarubens@users.noreply.github.com>
2025-04-28 11:35:17 -0400

    higher-dim conv, now without genAI slop

---
+++ higher_dim_conv.py
@@ -0,0 +1,60 @@
+import numpy as np
+from numpy.fft import fft2,ifft2
+from matplotlib import pyplot as plt
+import time
+
+def higher_dim_conv(f,g): # NEED TO ZERO-PAD THE ARRAYS ... in all directions, I think?!
+    # print("f.shape=",f.shape)
+    # print("g.shape=",g.shape)
+    if (f.shape!=g.shape):
+        if(f.shape!=(g.T).shape):
+            assert(1==0), "incompatible array shapes"
+        else: # need to transpose g for things to work as expected
+            g=g.T
+    a,b=f.shape # if eval makes it to this point, f.shape==g.shape is True
+    fp=np.zeros((2*a,2*b)) # holders for padded versions (wraparound-safe...)
+    gp=np.zeros((2*a,2*b))
+    fp[:a,:b]=f # populate the zero-padded versions
+    gp[:a,:b]=g
+    # print("fp.shape=",fp.shape)
+    # print("gp.shape=",gp.shape)
+
+    Fp=fft2(fp)
+    Gp=fft2(gp)
+    # print("Fp.shape=",Fp.shape)
+    # print("Gp.shape=",Gp.shape)
+    Fourier_space_product_p=Fp*Gp
+    # print("(Fp*Gp).shape=",Fourier_space_product_p.shape)
+    result_p=ifft2(Fourier_space_product_p)
+    # print("result.shape=",result_p.shape)
+    result=result_p[:a,:b].real
+    # result=np.flip(np.flip(result,0),1) # LOOKS LIKE IT NEEDS TO HAPPEN, BUT HOW TO MOTIVATE THIS MATHEMATICALLY??
+    return result.real
+
+A, B = 1010, 327
+Avec=np.arange(A)
+Bvec=np.arange(B)
+Plike=np.outer(np.exp(-(Avec/(0.2*A))**2),np.exp(-(Bvec/(0.2*B))**2)) # fills the role of W[deltakpar,deltakperp]
+print("Plike.shape",Plike.shape)
+Barr,Aarr=np.meshgrid(Bvec,Avec)
+f2=np.exp(-(Barr/(0.1*B))**2)
+print("f2.shape=",f2.shape)
+t0=time.time()
+windowed=higher_dim_conv(f2,Plike)
+t1=time.time()
+print("convolution-based windowing took {:6.4} s".format(t1-t0))
+print("windowed.shape=",windowed.shape)
+
+fig,axs=plt.subplots(2,2)
+im=axs[0,0].imshow(f2,       extent=[0,B,A,0],aspect=B/A)
+plt.colorbar(im,ax=axs[0,0])
+axs[0,0].set_title("W[$\Delta$ k$_{||}$,$\Delta$ k$_\perp$]")
+im=axs[0,1].imshow(Plike,    extent=[0,B,A,0],aspect=B/A)
+plt.colorbar(im,ax=axs[0,1])
+axs[0,1].set_title("P[k'$_{||}$,k'$_\perp$]")
+im=axs[1,0].imshow(windowed, extent=[0,B,A,0],aspect=B/A)
+plt.colorbar(im,ax=axs[1,0])
+axs[1,0].set_title("Pcont[k$_{||}$,k$_\perp$]")
+plt.tight_layout()
+plt.savefig("check_fft_cheat_mult.png")
+plt.show()
