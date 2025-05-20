1f6da70f9a38778b0f698cb41d0fcf5ea22f743a
Sophia Rubens <116817240+sophiarubens@users.noreply.github.com>
2025-05-20 10:52:45 -0400

    optimizing bin stats in power spectrum driver

--- power.py
+++ power.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import time
 
 '''
 this module helps connect ensemble-averaged power spectrum estimates and cosmological brighness temperature boxes for two main use cases:
@@ -14,7 +15,7 @@ call structure:
 {{}} = generally no need to call directly; called as necessary by higher-level functions
 '''
 
-def P(T, k, Lsurvey):
+def P(T, k, Lsurvey, binto="sph"):
     '''
     philosophy:
     * generate a power spectrum consistent with a brightness temperature box for which you already know the k-bins
@@ -25,20 +26,24 @@ def P(T, k, Lsurvey):
     inputs:
     T       = Npix x Npix x Npix data box for which you wish to create the power spectrum
     k       = k-bins for power spectrum indexing ... designed to be provided by one of two wrapper functions: user-created (custom) bins or automatically generated (linear- or log-spaced) bins
+                 - if binto=="sph": k is assumed to have shape (Nk,) or similar (e.g. (Nk,1) or (1,Nk))
+                 - if binto=="cyl": k is assumed to be a (possibly ragged) array that can be unpacked as kpar,kperp=k where kpar has shape (Nkpar,) (or similar) and kperp has shape (Nkperp,) or similar
     Lsurvey = side length of the simulation cube (Mpc) (just sets the volume element scaling... nothing to do with pixelization)
+    binto   = how to bin the resulting power spectrum (current options: spherically (1D) or cylindrically (2D, flat-sky approximation))
 
     outputs:
-    Npix x 2 array storing kbin,P(kbin) ordered pairs
+    k-modes and powers of the power spectrum
+         - if binto=="sph": [(Nk,),(Nk,)] object unpackable as kbins,powers
+         - if binto=="cyl": [(Nkpar,Nkperp),(Nkpar,Nkperp),(Nkpar,),(Nkperp,)] object unpackable as kgrid,powers,kparvec,kperpvec
     '''
     # helper variables
-    Nk=len(k)
     # print("T.shape=",T.shape)
-    # print("Nk=",Nk)
     Npix=T.shape[0]
     Delta = Lsurvey/Npix # voxel side length
     # print("P: Lsurvey,Npix,Delta=",Lsurvey,Npix,Delta)
     dr3 = Delta**3 # size of voxel
     twopi=2*np.pi
+    V = Lsurvey**3 # volume of the simulation cube
     
     # process the box values
     Ts = np.fft.ifftshift(T)*dr3 # T-ishifted (np wants a corner origin; ifftshift takes you there)
@@ -46,28 +51,103 @@ def P(T, k, Lsurvey):
     Tt = np.fft.fftshift(Tts) # shift back to physics land
     mTt = Tt*np.conjugate(Tt) # mod-squared of Tt
     mTt = mTt.real # I checked, and there aren't even any machine precision issues in the imag part
-    
-    # prepare to tie the processed box values to relevant k-values (generate k-quantities)
+
+    # establish Cartesian Fourier duals to box coordinates
     Kshuf=twopi*np.fft.fftshift(np.fft.fftfreq(Npix,d=Delta)) # fftshift doesn't care whether you're dealing with correlations, brightness temps, or config/Fourier space coordinates ... the math is the same!
     KX,KY,KZ=np.meshgrid(Kshuf,Kshuf,Kshuf)
-    kmags=np.sqrt(KX**2+KY**2+KZ**2) # literally just the Fourier duals to the config space points ... not the binned k-values yet
-    binidxs=np.digitize(kmags,k,right=False)
+    ## PROBABLY NEED TO ADD AN INDEXING KEYWORD WHICH BECOMES IMPORTANT FOR THE CASE OF CYLINDRICAL SYMMETRY BECAUSE THE INDICES ARE NO LONGER ALL INTERCHANGEABLE
+    
+    if (binto=="sph"):
+        Nk=len(k)
 
-    # identify which box each point falls into
-    amTt=np.zeros(Nk)
-    for i,binedge in enumerate(k):
-        here=np.nonzero(i==binidxs) # want all box indices where the corresp bin index is the ith binedge (nonzero is like argwhere, but gives output that is suitable for indexing arrays)
-        if (len(here[0])>0): # already know len(here) = arr_dimns, so check for emptiness w/ len(here[0])
-            amTt[i]=np.mean(mTt[here])
-        else:
-            amTt[0]=0
+        # prepare to tie the processed box values to relevant k-values
+        kmags=np.sqrt(KX**2+KY**2+KZ**2) # BINNING SPHERICALLY literally just the Fourier duals to the config space points ... not the binned k-values yet
+        print("np.min(kmags),np.max(kmags),k=",np.min(kmags),np.max(kmags),k)
 
-    V = Lsurvey**3 # volume of the simulation cube
-    k=np.array(k)
-    P=np.array(amTt/V)
-    return [k,P]
+        t0=time.time()
+        ##### START OF THE V4 APPROACH WHICH IS SIMPLE LIKE V1 BUT MORE VECTORIZED
+        binidxs=np.digitize(kmags,k,right=False)
+        binidxs_1d=np.reshape(binidxs,(Npix**3,))
+        mTt_1d=    np.reshape(mTt,    (Npix**3,))
+        summTt=np.bincount(binidxs_1d,weights=mTt_1d,minlength=Nk)
+        NmTt=  np.bincount(binidxs_1d,               minlength=Nk)
+        # amTt=summTt/NmTt # simple, but not safe against division-by-zero errors
+        amTt=np.zeros(Nk)
+        nonemptybins=np.nonzero(NmTt)
+        amTt[nonemptybins]=summTt[nonemptybins]/NmTt[nonemptybins]
+        #####
 
-def ps_userbin(T, kbins, Lsurvey):
+        # ##### START OF THE V3 SPEED-ORIENTED APPROACH
+        # summTt=np.zeros(Nk)
+        # NmTt=np.zeros(Nk)
+        # binidxs=np.digitize(kmags,k,right=False)
+        # for ii in range(Npix):
+        #     for jj in range(Npix):
+        #         for kk in range(Npix):
+        #             bin_of_current_box_val=binidxs[ii,jj,kk]
+        #             summTt[bin_of_current_box_val]+=mTt[ii,jj,kk]
+        #             NmTt[bin_of_current_box_val]+=1
+        # amTt=summTt/NmTt
+        # ##### END OF THIS V3 APPROACH
+
+        # ##### START OF THE V2 = "SEND EACH POINT INTO THE CORRECT BIN WHEN YOU FIRST ENCOUNTER IT" APPROACH
+        # summTt=np.zeros(Nk) # sum of mTt values that go into each bin
+        # NmTt=np.zeros(Nk) # number of mTt values that go into each bin
+        # for ii in range(Npix):
+        #     for jj in range(Npix):
+        #         for kk in range(Npix):
+        #             binidx=np.searchsorted(k,kmags[ii,jj,kk]) # there's no getting around the fact that I *will* have to check which bin something belongs in
+        #             summTt[binidx]+=mTt[ii,jj,kk]
+        #             NmTt[binidx]+=1
+        # # NmTt[NmTt==0]=0
+        # amTt=summTt/NmTt
+        # ##### END OF THIS V2 APPROACH
+
+        # ##### START OF THE V1 "TOO MANY CONVEYOR BELT TRIPS" APPROACH
+        # binidxs=np.digitize(kmags,k,right=False)
+
+        # # identify which box each point falls into
+        # amTt=np.zeros(Nk)
+        # for i,binedge in enumerate(k):
+        #     here=np.nonzero(i==binidxs) # want all box indices where the corresp bin index is the ith binedge (nonzero is like argwhere, but gives output that is suitable for indexing arrays)
+        #     if (len(here[0])>0): # already know len(here) = arr_dimns, so check for emptiness w/ len(here[0])
+        #         amTt[i]=np.mean(mTt[here]) # otherwise: leave as zero
+        # ##### END OF THIS OLD APPROACH
+        t1=time.time()
+        print("P: binning took",t1-t0,"s")
+        P=np.array(amTt/V)
+        return [k,P]
+    elif (binto=="cyl"):
+        # print("in the binto=='cyl' branch of P: ")
+        kpar,kperp=k # this assumes my apparently-nontraditional convention of putting kpar first... fix this later, probably
+        Nkpar=len(kpar)
+        Nkperp=len(kperp)
+        # print("Nkpar,Nkperp=",Nkpar,Nkperp)
+
+        # prepare to tie the processed box values to relevant k-values
+        kparmags=KZ
+        kperpmags=np.sqrt(KX**2+KY**2)
+        parbinidxs =np.digitize(kparmags, kpar, right=False)
+        perpbinidxs=np.digitize(kperpmags,kperp,right=False)
+        print("parbinidxs.min(),parbinidxs.max()=",parbinidxs.min(),parbinidxs.max())
+        print("perpbinidxs.min(),perpbinidxs.max()=",perpbinidxs.min(),perpbinidxs.max())
+
+        # identify which point each box falls into
+        amTt=np.zeros((Nkpar,Nkperp))
+        for i,parbinedge in enumerate(kpar):
+            for j,perpbinedge in enumerate(kperp):
+                here=np.nonzero(np.logical_and(i==parbinidxs,j==perpbinidxs)) # need to verify this generalization to 2D
+                if (len(here[0])>0):
+                    amTt[i,j]=np.mean(mTt[here])
+        kpargrid,kperpgrid=np.meshgrid(kpar,kperp,indexing="ij") # need to verify that the indexing actually does what I mean for it to do
+        P=np.array(amTt/V)
+        # print("before return: kpargrid.shape,kperpgrid.shape,P.shape,kpar.shape,kperp.shape=",kpargrid.shape,kperpgrid.shape,P.shape,kpar.shape,kperp.shape)
+        return [[kpargrid,kperpgrid],P,kpar,kperp]
+    else:
+        assert(1==0), "only spherical and cylindrical power spectrum binning are currently supported"
+        return None
+
+def ps_userbin(T, kbins, Lsurvey, binto="sph"):
     '''
     philosophy: 
     * generate a power spectrum with user-provided bins, consistent with a given brightness temperature box
@@ -76,33 +156,51 @@ def ps_userbin(T, kbins, Lsurvey):
 
     inputs:
     T       = Npix x Npix x Npix data box for which you wish to create the power spectrum
-    kbins   = user-defined (if using this wrapper function, then probably custom-spaced) VECTOR of k-bins
+    kbins   = user-defined (if using this wrapper function, then probably custom-spaced) k-bins
+                - if binto=="sph": vector of shape (Nk,) or similar
+                - if binto=="cyl": potentially ragged array [(Nkpar,),(Nkperp,)] or similar, unpackable as kpar,kperp=kbins
     Lsurvey = side length of the simulation cube (Mpc) (just sets the volume element scaling... nothing to do with pixelization)
 
     outputs:
-    Npix x 2 array storing kbin,P(kbin) ordered pairs
+    one copy of P() output
     '''  
-    Npix = T.shape[0]
-    kmax=kbins[-1]
-    kmin=kbins[0]
-    if (kmax<kmin):
-        kbins=np.flip(kbins)
+    Npix = T.shape[0] # assumes that you use the function as intended and pass a data cube
+    if (binto=="sph"):
         kmax=kbins[-1]
         kmin=kbins[0]
-    elif (kmax==kmin):
-        assert(1==0), "kbins is either constant or non-monotonic -> cannot create useful bins from these" # force quit ... the always false condition is just a hanger for the warning message
-    twopi=2*np.pi
-    Delta=Lsurvey/Npix
-    # print("kmax,kmin,twopi/Delta,twopi/Lsurvey=",kmax,kmin,twopi/Delta,twopi/Lsurvey)
-    # assert (kmax<=twopi/Delta and kmin>=twopi/Lsurvey), 'the provided k-range must be a subset of the range probed by this survey'
-    Nk=len(kbins)
-    return P(T,kbins,Lsurvey)
+        if (kmax<kmin):
+            kbins=np.flip(kbins)
+            # kmax=kbins[-1] # not actually used anywhere ... leave commented for now to make sure things behave as expected/ that I'm not missing anything before actually removing
+            # kmin=kbins[0]
+        elif (kmax==kmin):
+            assert(1==0), "constant or non-monotonic k bins -> try again with more sensible binning" # force quit ... the always false condition is just a hanger for the warning message
+        twopi=2*np.pi
+        Delta=Lsurvey/Npix
+        Nk=len(kbins)
+    elif (binto=="cyl"):
+        kpar,kperp=kbins
+        kparmin,kparmax=kpar[0],kpar[-1]
+        kperpmin,kperpmax=kperp[0],kperp[-1]
+        if (kparmax<kparmin):
+            kpar=np.flip(kbins)
+        elif (kparmax==kparmin):
+            assert(1==0), "constant or non-monotonic kpar bins -> try again with more sensible binning"
+        if (kperpmax<kperpmin):
+            kperp=np.flip(kperp)
+        elif(kperpmax==kperpmin):
+            assert(1==0), "constant or non-monotonic kperp bins -> try again with more sensible binning"
+        kbins=[kpar,kperp]
+    else:
+        assert(1==0), "only spherical and cylindrical power spectrum binning are currently supported"
+
+    return P(T,kbins,Lsurvey, binto=binto)
 
 def ps_autobin(T, mode, Lsurvey, Nk):
     '''
     philosophy:
-    * generate a power spectrum with lin- or log-spaced bins, consistent with a given brightness temperature box
+    * generate a spherically binned power spectrum with lin- or log-spaced bins, consistent with a given brightness temperature box
     * wrapper function for the power spectrum function P(T, k, Lsurvey, Npix) 
+    * I could generalize this to calculate cylindrical bins accessed by a particular instrument, but I'd need to pass a slew of survey parameters to this function, and for now it seems cleaner to precalculate them the way you want and then pass to the custom bin wrapper
 
     inputs:
     T       = Npix x Npix x Npix data box for which you wish to create the power spectrum
@@ -111,18 +209,21 @@ def ps_autobin(T, mode, Lsurvey, Nk):
     Nk      = number of k-bins to include in the power spectrum
 
     outputs:
-    Npix x 2 array storing kbin,P(kbin) ordered pairs
+    one copy of P() output
     '''
     Npix=T.shape[0]
     twopi=2*np.pi
     Delta=Lsurvey/Npix
     assert (mode=='lin' or mode=='log'), 'only linear and logarithmic auto-generated bins are currently supported'
+    
     kmax=twopi/Delta # was Npix but I don't think that makes sense anymore
     kmin=twopi/Lsurvey
     if (mode=='log'):
-        kbins=np.logspace(np.log10(kmin),np.log10(kmax),num=Nk+1)[:-1] # bypass the poor stats for the largest k-bin
+        # kbins=np.logspace(np.log10(kmin),np.log10(kmax),num=Nk+1)[:-1] # bypass the poor stats for the largest k-bin
+        kbins=np.logspace(np.log10(kmin),np.log10(kmax),num=Nk)
     else:
-        kbins=np.linspace(kmin,kmax,Nk+1)[:-1]
+        # kbins=np.linspace(kmin,kmax,Nk+1)[:-1]
+        kbins=np.linspace(kmin,kmax,Nk)
     return P(T,kbins,Lsurvey)
 
 def flip(n,nfvox):
@@ -224,27 +325,64 @@ def ips(P,k,Lsurvey,nfvox):
     
     return rgrid,T,rmags
 
-# ############## TESTS FWD
-# Lsurvey = 103
-# Npix = 99
-# Nk = 12
+############## TEST SPH FWD
+Lsurvey = 103
+Npix = 99
+Nk = 12
 
 # kcustom=np.logspace(np.log10(2.*np.pi/100),np.log10(5),Nk)
+plt.figure()
+maxvals=0.0
+for i in range(10):
+    T = np.random.normal(loc=0.0, scale=1.0, size=(Npix,Npix,Npix))
+    # kfloors,vals=ps_userbin(T,kcustom,Lsurvey)
+    kfloors,vals=ps_autobin(T,"log",Lsurvey,Nk)
+    plt.scatter(np.log(kfloors),vals)
+    maxvalshere=np.max(vals)
+    if (maxvalshere>maxvals):
+        maxvals=maxvalshere
+plt.xlabel("log(k*1Mpc)")
+plt.ylabel("Power (K$^2$ Mpc$^3$)")
+plt.title("Test PS calc for Lsurvey,Npix,Nk={:4},{:4},{:4}".format(Lsurvey,Npix,Nk))
+plt.ylim(0,1.2*maxvals)
+plt.show()
 
-# plt.figure()
-# maxvals=0.0
-# for i in range(10):
-#     T = np.random.normal(loc=0.0, scale=1.0, size=(Npix,Npix,Npix))
-#     kfloors,vals=ps_userbin(T,kcustom,Lsurvey) # ps_userbin(T, kbins, Lsurvey)
-#     # kfloors,vals=ps_autobin(T,"log",Lsurvey,Nk)
-#     plt.scatter(np.log(kfloors),vals)
-#     maxvalshere=np.max(vals)
-#     if (maxvalshere>maxvals):
-#         maxvals=maxvalshere
-# plt.xlabel("log(k*1Mpc)")
-# plt.ylabel("Power (K$^2$ Mpc$^3$)")
-# plt.title("Test PS calc for Lsurvey,Npix,Nk={:4},{:4},{:4}".format(Lsurvey,Npix,Nk))
-# plt.ylim(0,1.2*maxvals)
+# ############## TEST CYL FWD
+# Lsurvey = 8
+# # Nkpar = 327
+# # Nkperp = 1010
+# Nkpar=12
+# Nkperp=7
+
+# kperp=np.linspace(0.09,3.3,Nkperp)
+# kpar=np.linspace(0.02,6.1,Nkpar)
+# deltakpar=kpar[1]-kpar[0]
+# deltakperp=kperp[1]-kperp[0]
+# largerdeltak=np.max((deltakpar,deltakperp))
+
+# Npix = 99 # bad bc things get skipped
+# kcustom=[kpar,kperp]
+# nsubrow=3
+# nsubcol=3
+# vmin=np.infty
+# vmax=-np.infty
+# fig,axs=plt.subplots(nsubrow,nsubcol)
+# for i in range(nsubrow):
+#     for j in range(nsubcol):
+#         T = np.random.normal(loc=0.0, scale=1.0, size=(Npix,Npix,Npix))
+#         [kpargrid_returned,kperpgrid_returned],vals,kpar_returned,kperp_returned=ps_userbin(T,kcustom,Lsurvey,binto="cyl")
+#         im=axs[i,j].imshow(vals,origin="lower",extent=[kpar[0],kpar[-1],kperp[0],kperp[-1]])
+#         axs[i,j].set_ylabel("$k_{||}$")
+#         axs[i,j].set_xlabel("$k_\perp$")
+#         minval=np.min(vals)
+#         maxval=np.max(vals)
+#         if (minval<vmin):
+#             vmin=minval
+#         if (maxval>vmax):
+#             vmax=maxval
+# fig.colorbar(im,extend="both")
+# plt.suptitle("Test cyl PS calc")
+# plt.tight_layout()
 # plt.show()
 
 ############## TESTS BWD
