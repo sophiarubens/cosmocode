fixed NumericalDeltaError, more realiz avging intu

bias_helper_fcns.py
@@ -49,6 +49,9 @@ args not explained here will be documented in the single function in which they 
 class NvoxPracticalityError(Exception):
     pass
 
+class NumericalDeltaError(Exception):
+    pass
+
 def get_channel_config(nu_ctr,Deltanu,evol_restriction_threshold=1./15.):
     """
     args
@@ -61,7 +64,7 @@ def get_channel_config(nu_ctr,Deltanu,evol_restriction_threshold=1./15.):
     N        = number survey channels
     """
     NDeltanu=nu_ctr*evol_restriction_threshold
-    N=NDeltanu/Deltanu
+    N=int(NDeltanu/Deltanu)
     return NDeltanu,N
 
 def ceilflex(x,roundto=0.05):
@@ -184,7 +187,22 @@ def calc_Pcont_asym(pars_set_cosmo,z,kpar,kperp,sigLoS,epsLoS,r0,beamfwhm_x,beam
     t1=time.time()
     print(">> Pcont calc: sourced pspec from CAMB",t1-t0)
     Lsurvbox,Nvoxbox=get_L_N_for_box(kpar,kperp)
-    # print("Lsurvbox,Nvoxbox=",Lsurvbox,Nvoxbox)
+    Deltabox=Lsurvbox/Nvoxbox
+    sky_plane_sigmas=r0*np.array([beamfwhm_x,beamfwhm_y])/np.sqrt(2*np.log(2))
+    all_sigmas=np.concatenate((sky_plane_sigmas,[sigLoS]))
+    print("IN CALC_PCONT_ASYM: sky_plane_sigmas,sigLoS,Deltabox=",sky_plane_sigmas,sigLoS,Deltabox)
+    # print("sky_plane_sigmas[0]<Deltabox:",sky_plane_sigmas[0]<Deltabox)
+    # print("sky_plane_sigmas[1]<Deltabox:",sky_plane_sigmas[1]<Deltabox)
+    # print("(np.any(sky_plane_sigmas)<Deltabox):",(np.any(sky_plane_sigmas)<Deltabox))
+    # print("(sigLoS<Deltabox):",(sigLoS<Deltabox))
+    # print("np.any(np.concatenate(((sky_plane_sigmas<Deltabox),(np.array(sigLoS<Deltabox))))):",np.any(np.concatenate(((sky_plane_sigmas<Deltabox),(np.array(sigLoS<Deltabox))))))
+    # print("(np.any(sky_plane_sigmas)<Deltabox) or (sigLoS<Deltabox):",(np.any(sky_plane_sigmas)<Deltabox) or (sigLoS<Deltabox))
+    print("np.any(all_sigmas<Deltabox):",np.any(all_sigmas<Deltabox))
+    if (np.any(all_sigmas<Deltabox)):
+        print("entering if branch bc there is a sigma that makes things delta-y")
+        raise NumericalDeltaError
+    else:
+        print("sigmas all okay... eval should continue")
     nkpar=len(kpar)
     nkperp=len(kperp)
     Ptrue_realizations=   np.zeros((nkpar,nkperp,n_realiz))
@@ -245,43 +263,43 @@ def custom_response(X,Y,Z,sigLoS,beamfwhm_x,beamfwhm_y,r0):
     (Nvox,Nvox,Nvox) Cartesian box (z=LoS direction), centred at r0, sampling the response fcn at each point
     """
     response=np.exp(-(Z/(2*sigLoS))**2 -ln2*((X/beamfwhm_x)**2+(Y/beamfwhm_y)**2)/r0**2)
-    fig,axs=plt.subplots(3,3,figsize=(15,8))
-    Nvox=X.shape[0]
-    half=Nvox//2
-    axs[0,0].pcolormesh(X[:,:,0   ],Y[:,:,0   ],response[:,:,0   ])
-    axs[0,0].set_title("response[:,:,0]")
-    axs[0,1].pcolormesh(X[:,:,half],Y[:,:,half],response[:,:,half])
-    axs[0,1].set_title("response[:,:,half]")
-    axs[0,2].pcolormesh(X[:,:,-1  ],Y[:,:,-1  ],response[:,:,-1  ])
-    axs[0,2].set_title("response[:,:,-1]")
-    for i in range(3):
-        axs[0,i].set_xlabel("x")
-        axs[0,1].set_ylabel("y")
+    # fig,axs=plt.subplots(3,3,figsize=(15,8))
+    # Nvox=X.shape[0]
+    # half=Nvox//2
+    # axs[0,0].imshow(response[:,:,0   ])
+    # axs[0,0].set_title("response[:,:,0]")
+    # axs[0,1].imshow(response[:,:,half])
+    # axs[0,1].set_title("response[:,:,half]")
+    # axs[0,2].imshow(response[:,:,-1  ])
+    # axs[0,2].set_title("response[:,:,-1]")
+    # for i in range(3):
+    #     axs[0,i].set_xlabel("x")
+    #     axs[0,1].set_ylabel("y")
 
-    axs[1,0].pcolormesh(X[:,0,   :],Z[:,0,   :],response[:,0,   :])
-    axs[1,0].set_title("response[:,0,:]")
-    axs[1,1].pcolormesh(X[:,half,:],Z[:,half,:],response[:,half,:])
-    axs[1,1].set_title("response[:,half,:]")
-    axs[1,2].pcolormesh(X[:,-1,  :],Z[:,-1,  :],response[:,-1,  :])
-    axs[1,2].set_title("response[:,-1,:]")
-    for i in range(3):
-        axs[1,i].set_xlabel("x")
-        axs[1,i].set_ylabel("z")
+    # axs[1,0].imshow(response[:,0,   :])
+    # axs[1,0].set_title("response[:,0,:]")
+    # axs[1,1].imshow(response[:,half,:])
+    # axs[1,1].set_title("response[:,half,:]")
+    # axs[1,2].imshow(response[:,-1,  :])
+    # axs[1,2].set_title("response[:,-1,:]")
+    # for i in range(3):
+    #     axs[1,i].set_xlabel("x")
+    #     axs[1,i].set_ylabel("z")
 
-    axs[2,0].pcolormesh(Y[0,   :,:],Z[0,   :,:],response[0,   :,:])
-    axs[2,0].set_title("response[0,:,:]")
-    axs[2,1].pcolormesh(Y[half,:,:],Z[half,:,:],response[half,:,:])
-    axs[2,1].set_title("response[half,:,:]")
-    axs[2,2].pcolormesh(Y[-1,  :,:],Z[-1,  :,:],response[-1,  :,:])
-    axs[2,2].set_title("response[-1,:,:]")
-    for i in range(3):
-        axs[2,i].set_xlabel("Y")
-        axs[2,i].set_xlabel("Z")
+    # axs[2,0].imshow(response[0,   :,:])
+    # axs[2,0].set_title("response[0,:,:]")
+    # axs[2,1].imshow(response[half,:,:])
+    # axs[2,1].set_title("response[half,:,:]")
+    # axs[2,2].imshow(response[-1,  :,:])
+    # axs[2,2].set_title("response[-1,:,:]")
+    # for i in range(3):
+    #     axs[2,i].set_xlabel("Y")
+    #     axs[2,i].set_xlabel("Z")
 
-    plt.suptitle("inspect response")
-    plt.tight_layout()
-    plt.savefig("inspect_response.png")
-    plt.show()
+    # plt.suptitle("inspect response")
+    # plt.tight_layout()
+    # plt.savefig("inspect_response.png")
+    # plt.show()
     return response # LoS direction is z-like! everything is compatible here! see the comments in the elif (binto=="cyl") branch of P_driver in power.py to remind myself!!
 
 def unbin_to_Pcyl(kpar,kperp,z,pars_set_cosmo=pars_set_cosmo_Planck18,n_sph_modes=500):  

cyl_window_tests.py
@@ -122,7 +122,7 @@ if verbose_test_prints: # fans of well-formatted print statements look away now.
 ############################## actual pipeline test ########################################################################################################################
 recalc_biases=True
 if recalc_biases:
-    calc_P_cyl_partials=True
+    calc_P_cyl_partials=False
     if calc_P_cyl_partials:
         P_cyl_partials=build_cyl_partials(pars_Planck18,z_ctr,n_sph_pts_test,kpar_surv,kperp_surv,dpar)
         np.save("P_cyl_partials.npy",P_cyl_partials)
@@ -152,7 +152,7 @@ if recalc_biases:
                         cyl_sym_resp=False, 
                         fwhmbeam1=beam_fwhm1, epsbeam1=epsbeam1_test,
                         recalc_Pcont=True,
-                        savename="cyl_asym", n_realiz=3) # n_realiz=1 recovers the previous case where I do not average over realizations
+                        savename="cyl_asym", n_realiz=50) # n_realiz=1 recovers the previous case where I do not average over realizations
     printparswbiases(pars_Planck18,parnames,b_cyl_asym_resp)
 
 ## debug zone to inspect the Pconts more closely for the two cases (this term is responsible for all the differences in the results between the two bias calc strategies at the moment)
@@ -234,9 +234,9 @@ for i in range(3):
         if (j==2):
             qty=" Ptrue"
         if (j==3):
-            qty=" slices with constant min k$_{||}$"
-        if (j==4):
             qty=" slices with constant min k$_\perp$"
+        if (j==4):
+            qty=" slices with constant min k$_{||}$"
         axs[i,j].set_title(case+qty)
         if (j<3):
             axs[i,j].set_xlabel("k$_{||}$ (1/Mpc)")

power_tests.py
@@ -3,6 +3,8 @@ from bias_helper_fcns import custom_response
 import time
 import numpy as np
 from matplotlib import pyplot as plt
+import time
+
 Lsurvey=126
 Npix=52
 mode="lin"
@@ -10,6 +12,9 @@ mode="lin"
 Nkpar=8 # 327
 Nkperp=12 # 1010
 Nk = 14
+
+def elbowy_power(k,a=0.96605,b=-0.8,c=1,a0=1,b0=5000):
+    return c/(a0*k**(-a)+b0*k**(-b))
 
 test_sph_fwd=True
 if test_sph_fwd:
@@ -112,24 +117,18 @@ if test_sph_interp:
     plt.show()
 
 test_cyl_fwd=True
+power_spec_type="wn"
+power_spec_type="bpl"
 if test_cyl_fwd:
-    nsubrow=3
-    nsubcol=3
-    vmin=np.infty
-    vmax=-np.infty
-
-
-    ##
     fig,axs=plt.subplots(4,4,figsize=(20,20))
     maxvals=0.
     maxvals_mod0=0.
     maxvals_mod1=0.
     maxvals_mod2=0.
-    Nrealiz=35
+    Nrealiz=100
 
     vec=1/np.fft.fftshift(np.fft.fftfreq(Npix,d=Lsurvey/Npix)) # based on k_vec_for_box=twopi*np.fft.fftshift(np.fft.fftfreq(Nvox,d=Delta)) and r=2pi/k
     xgrid,ygrid,zgrid=np.meshgrid(vec,vec,vec,indexing="ij")
-    # print("config space voxel scale = vec[1]-vec[0]=",vec[1]-vec[0])
     sigma02=1e3
     sigma12=10
     sigma22=0.5
@@ -141,30 +140,49 @@ if test_cyl_fwd:
     allvals1=np.zeros((Nkpar,Nkperp,Nrealiz))
     allvals2=np.zeros((Nkpar,Nkperp,Nrealiz))
 
+    tprev=time.time()
     for i in range(Nrealiz):
-        T = np.random.normal(loc=0.0, scale=1.0, size=(Npix,Npix,Npix))
+        if power_spec_type=="wn":
+            T = np.random.normal(loc=0.0, scale=1.0, size=(Npix,Npix,Npix))
+        elif power_spec_type=="bpl":
+            if (i==0):
+                Npts_bpl=25
+                k_bpl=np.linspace(twopi/Lsurvey,Npix*pi/Lsurvey,Npts_bpl)
+                P_bpl=elbowy_power(k_bpl)
+            _,T,_ = generate_box(P_bpl,k_bpl,Lsurvey,Npix) # generate_box(P,k,Lsurvey,Nvox,verbose=False) returns rgrid,T,rmags
         Tmod0=T*modulation0
-        # print("broad in config")
         kfloors_mod,vals_mod0=generate_P(Tmod0,mode,Lsurvey,Nkpar,Nk1=Nkperp,custom_estimator=custom_response,custom_estimator_args=(np.sqrt(sigma02),np.sqrt(sigma02),np.sqrt(sigma02),2*np.sqrt(np.log(2)),)) # ,sigLoS,beamfwhm_x,beamfwhm_y,r0)
-        # print("vals_mod0.shape=",vals_mod0.shape)
         allvals0[:,:,i]=vals_mod0
         Tmod1=T*modulation1
-        # print("medium in config")
         kfloors_mod,vals_mod1=generate_P(Tmod1,mode,Lsurvey,Nkpar,Nk1=Nkperp,custom_estimator=custom_response,custom_estimator_args=(np.sqrt(sigma12),np.sqrt(sigma12),np.sqrt(sigma12),2*np.sqrt(np.log(2)),))
         allvals1[:,:,i]=vals_mod1
         Tmod2=T*modulation2
-        # print("narrow in config")
         kfloors_mod,vals_mod2=generate_P(Tmod2,mode,Lsurvey,Nkpar,Nk1=Nkperp,custom_estimator=custom_response,custom_estimator_args=(np.sqrt(sigma22),np.sqrt(sigma22),np.sqrt(sigma22),2*np.sqrt(np.log(2)),))
         allvals2[:,:,i]=vals_mod2
         kfloors,vals=generate_P(T,mode,Lsurvey,Nkpar,Nk1=Nkperp,custom_estimator=custom_response,custom_estimator_args=(np.sqrt(sigma22),np.sqrt(sigma22),np.sqrt(sigma22),2*np.sqrt(np.log(2)),))
         allvals[:,:,i]=vals
+        if ((i%50)==0):
+            tcurr=time.time()
+            print("finished constructing realization ",i,"in",tcurr-tprev,"s")
+            tprev=tcurr
+    
+    np.save("allvals0"+power_spec_type+".npy",allvals0)
+    np.save("allvals1"+power_spec_type+".npy",allvals1)
+    np.save("allvals2"+power_spec_type+".npy",allvals2)
+    np.save("allvals"+power_spec_type+".npy",allvals)
     valmin=np.min(allvals)
     valmax=np.max(allvals)
     kparfloors,kperpfloors=kfloors
     kparfloorsgrid,kperpfloorsgrid=np.meshgrid(kparfloors,kperpfloors,indexing="ij")
 
     column_names=["unmod","mod broad in config sp","mod medium in config sp","mod narrow in config sp"]
-    row_names=["realiz 0","realiz 1","realiz 2","average of"+str(Nrealiz)+"realix ("+str(Nrealiz-3)+"realiz not shown)"]
+    row_names=["realiz 0","realiz 1","realiz 2","\navg of "+str(Nrealiz)+" realiz \n("+str(Nrealiz-3)+"realiz not shown)"]
+
+    maxunmod= np.max(allvals)
+    max0=     np.max(allvals0)
+    max1=     np.max(allvals1)
+    max2=     np.max(allvals2)
+
     for i in range(4):
         for j in range(4):
             axs[i,j].set_xlabel("k (1/Mpc)")
@@ -172,31 +190,70 @@ if test_cyl_fwd:
             axs[i,j].set_title(column_names[j]+" - "+row_names[i])
         if (i<3):
             # print("kparfloors.shape,kperpfloors.shape,allvals[:,:,i].shape=",kparfloors.shape,kperpfloors.shape,allvals[:,:,i].shape)
-            im=axs[i,0].pcolor(kparfloorsgrid,kperpfloorsgrid,allvals[:,:,i],  vmin=valmin,vmax=valmax)
+            im=axs[i,0].pcolor(kparfloorsgrid,kperpfloorsgrid,allvals[:,:,i],vmin=0,vmax=maxunmod)
             fig.colorbar(im,ax=axs[i,0])
-            im=axs[i,1].pcolor(kparfloorsgrid,kperpfloorsgrid,allvals0[:,:,i], vmin=valmin,vmax=valmax)
+            im=axs[i,1].pcolor(kparfloorsgrid,kperpfloorsgrid,allvals0[:,:,i],vmin=0,vmax=max0)
             fig.colorbar(im,ax=axs[i,1])
-            im=axs[i,2].pcolor(kparfloorsgrid,kperpfloorsgrid,allvals1[:,:,i], vmin=valmin,vmax=valmax)
+            im=axs[i,2].pcolor(kparfloorsgrid,kperpfloorsgrid,allvals1[:,:,i],vmin=0,vmax=max1)
             fig.colorbar(im,ax=axs[i,2])
-            im=axs[i,3].pcolor(kparfloorsgrid,kperpfloorsgrid,allvals2[:,:,i], vmin=valmin,vmax=valmax)
+            im=axs[i,3].pcolor(kparfloorsgrid,kperpfloorsgrid,allvals2[:,:,i],vmin=0,vmax=max2)
             fig.colorbar(im,ax=axs[i,3])
 
         
-
-    im=axs[3,0].pcolor(kparfloorsgrid,kperpfloorsgrid,np.mean(allvals,axis=-1),  vmin=valmin,vmax=valmax)
+    mean=  np.mean(allvals,axis=-1)
+    mean0= np.mean(allvals0,axis=-1)
+    mean1= np.mean(allvals1,axis=-1)
+    mean2= np.mean(allvals2,axis=-1)
+    im=axs[3,0].pcolor(kparfloorsgrid,kperpfloorsgrid,mean,vmin=0,vmax=maxunmod)
     fig.colorbar(im,ax=axs[3,0])
-    im=axs[3,1].pcolor(kparfloorsgrid,kperpfloorsgrid,np.mean(allvals0,axis=-1), vmin=valmin,vmax=valmax)
-    fig.colorbar(im,ax=axs[3,0])
-    im=axs[3,2].pcolor(kparfloorsgrid,kperpfloorsgrid,np.mean(allvals1,axis=-1), vmin=valmin,vmax=valmax)
-    fig.colorbar(im,ax=axs[3,0])
-    im=axs[3,3].pcolor(kparfloorsgrid,kperpfloorsgrid,np.mean(allvals2,axis=-1), vmin=valmin,vmax=valmax)
-    fig.colorbar(im,ax=axs[3,0])
+    im=axs[3,1].pcolor(kparfloorsgrid,kperpfloorsgrid,mean0,vmin=0,vmax=max0)
+    fig.colorbar(im,ax=axs[3,1])
+    im=axs[3,2].pcolor(kparfloorsgrid,kperpfloorsgrid,mean1,vmin=0,vmax=max1)
+    fig.colorbar(im,ax=axs[3,2])
+    im=axs[3,3].pcolor(kparfloorsgrid,kperpfloorsgrid,mean2,vmin=0,vmax=max2)
+    fig.colorbar(im,ax=axs[3,3])
     # fig.colorbar(im)
 
-    plt.suptitle("Test white noise P(kpar,kperp) calc for Lsurvey,Npix,Nkpar,Nkperp,sigma0**2,sigma1**2,sigma2**2={:4},{:4},{:4},{:4},{:4},{:4},{:4}".format(Lsurvey,Npix,Nkpar,Nkperp,sigma02,sigma12,sigma22))
-    
+    plt.suptitle("Test "+power_spec_type+" P(kpar,kperp) calc for Lsurvey,Npix,Nkpar,Nkperp,sigma0**2,sigma1**2,sigma2**2={:4},{:4},{:4},{:4},{:4},{:4},{:4}".format(Lsurvey,Npix,Nkpar,Nkperp,sigma02,sigma12,sigma22))
+    for i in range(4):
+        for j in range(4):
+            axs[i,j].set_aspect("equal")
     plt.tight_layout()
-    plt.savefig("wn_cyl_"+mode+".png",dpi=500)
+    plt.savefig(power_spec_type+"_cyl_mod_"+mode+"_"+str(Nrealiz)+"_realiz.png",dpi=500)
+    plt.show()
+
+    fig,axs=plt.subplots(2,4,figsize=(20,10))
+    im=axs[0,0].pcolor(kparfloorsgrid,kperpfloorsgrid,mean, vmin=0)
+    fig.colorbar(im,ax=axs[0,0])
+    im=axs[0,1].pcolor(kparfloorsgrid,kperpfloorsgrid,mean0,vmin=0)
+    fig.colorbar(im,ax=axs[0,1])
+    im=axs[0,2].pcolor(kparfloorsgrid,kperpfloorsgrid,mean1,vmin=0)
+    fig.colorbar(im,ax=axs[0,2])
+    im=axs[0,3].pcolor(kparfloorsgrid,kperpfloorsgrid,mean2,vmin=0)
+    fig.colorbar(im,ax=axs[0,3])
+
+    meanmean=np.mean(mean)
+    meanmean0=np.mean(mean0)
+    meanmean1=np.mean(mean1)
+    meanmean2=np.mean(mean2)
+    im=axs[1,0].pcolor(kparfloorsgrid,kperpfloorsgrid,(mean-meanmean)/meanmean,   vmin=0)
+    fig.colorbar(im,ax=axs[1,0])
+    im=axs[1,1].pcolor(kparfloorsgrid,kperpfloorsgrid,(mean0-meanmean0)/meanmean0,vmin=0)
+    fig.colorbar(im,ax=axs[1,1])
+    im=axs[1,2].pcolor(kparfloorsgrid,kperpfloorsgrid,(mean1-meanmean1)/meanmean1,vmin=0)
+    fig.colorbar(im,ax=axs[1,2])
+    im=axs[1,3].pcolor(kparfloorsgrid,kperpfloorsgrid,(mean2-meanmean2)/meanmean2,vmin=0)
+    fig.colorbar(im,ax=axs[1,3])
+    row_names_2=["\navg over realizations","\nfractional residual"]
+    for i in range(2):
+        for j in range(4):
+            axs[i,j].set_aspect("equal")
+            axs[i,j].set_xlabel("k (1/Mpc)")
+            axs[i,j].set_ylabel("P (K$^2$ Mpc$^3$)")
+            axs[i,j].set_title(column_names[j]+row_names_2[i])
+    plt.suptitle("examine fractional residuals of means over "+str(Nrealiz)+"realizations")
+    plt.tight_layout()
+    plt.savefig(power_spec_type+"_cyl_mod_"+mode+"_"+str(Nrealiz)+"_frac_resids.png",dpi=500)
     plt.show()
 
 test_cyl_interp=False