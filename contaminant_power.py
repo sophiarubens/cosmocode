f8b381bf2c51583e0b3d50b210cf4a7fbe71ab0b
Sophia Rubens <116817240+sophiarubens@users.noreply.github.com>
2025-10-09 10:33:12 -0400

    sensible presentation of cont plots

--- contaminant_power.py
+++ contaminant_power.py
@@ -1,4 +1,5 @@
 import numpy as np
+# from matplotlib.colors import LinearSegmentedColormap
 from matplotlib import pyplot as plt
 from scipy.interpolate import interpn
 from cosmo_distances import *
@@ -44,92 +45,112 @@ bminCHORD=6.3
 bmaxCHORD=np.sqrt((b_NS_CHORD*10)**2+(b_EW_CHORD*7)**2) # pathfinder (as per the CHORD-all telecon on May 26th, but without holes)
 kperp_surv=kperp(nu_ctr,N_CHORDbaselines,bminCHORD,bmaxCHORD) # kperp(nu_ctr,N_modes,bmin,bmax)
 ceil=275
+# ceil=0 # takes way too long to evaluate
+# ceil=200 # apparently also takes way too long to evaluate
+# ceil=230 # 485 s for the test to run (in the form it was in around 09:00 on Thursday.)
 
 n_sph_nu=250
 
 hpbw_x= 6*  pi/180. # rad; lambda/D estimate (actually physically realistic)
 hpbw_y= 4.5*pi/180.
 
-epsLoS=0.1
-epsxy=0.1
-# epsx=0.1
-# epsy=0.1
-
-##############################
-bundled_gaussian_primary_args=[hpbw_x,hpbw_y]
-bundled_gaussian_primary_uncs=[epsLoS,epsxy,epsxy]
-
+# epsilons_xy=[0.05]
+# epsilons_xy=[0.05,0.3]
+epsilons_xy=np.arange(0.0,0.2,0.05)
+N_systematic_cases=len(epsilons_xy)
+blues_here = plt.cm.Blues( np.linspace(1,0.2,N_systematic_cases))
 redo_window_calc=False
-nu_window=window_calcs(bminCHORD,bmaxCHORD,
-                        ceil,
-                        "AiryGaussian",bundled_gaussian_primary_args,bundled_gaussian_primary_uncs,
-                        pars_Planck18,pars_Planck18,
-                        n_sph_nu,dpar,
-                        nu_ctr,channel_width,
-                        pars_forecast_names=parnames, no_monopole=False)
-nu_window.print_survey_characteristics()
-if redo_window_calc:
-    t0=time.time()
-    nu_window.calc_Pcont_asym()
-    t1=time.time()
-    print("Pcont calculation time was",t1-t0)
-
-    Pcont_cyl_surv=nu_window.Pcont_cyl_surv
-    # need to interpolate to survey modes even though I'm not interested in the whole bias calculation... try to short-circuit? changed in pipeline for now
-    Pthought_cyl_surv=nu_window.Pthought_cyl_surv
-    Pfidu_sph=nu_window.Ptruesph # self.ksph,self.Ptruesph
-    kfidu_sph=nu_window.ksph
-
-    np.save("Pcont_cyl_surv.npy",Pcont_cyl_surv)
-    np.save("Pthought_cyl_surv.npy",Pthought_cyl_surv)
-    np.save("Pfidu_sph.npy",Pfidu_sph)
-    np.save("kfidu_sph.npy",kfidu_sph)
-else:
-    Pcont_cyl_surv=np.load("Pcont_cyl_surv.npy")
-    Pthought_cyl_surv=np.load("Pthought_cyl_surv.npy")
-    Pfidu_sph=np.load("Pfidu_sph.npy")
-    kfidu_sph=np.load("kfidu_sph.npy")
-
-N_sph=128
-kmin_surv=nu_window.kmin_surv
-kmax_surv=nu_window.kmax_surv
-k_sph=np.linspace(kmin_surv,kmax_surv,N_sph)
-kpar=nu_window.kpar_surv
-kperp=nu_window.kperp_surv
-k00,k11=np.meshgrid(kpar,kperp,indexing="ij")
-kcyl_for_interp=(kpar,kperp)
-Pfidu_sph=np.reshape(Pfidu_sph,(Pfidu_sph.shape[-1],))
-
-Pcont_sph=interpn(kcyl_for_interp, Pcont_cyl_surv, k_sph, bounds_error=False, fill_value=None)
-doubled=np.linspace(kmin_surv,kmax_surv,N_sph*2) # print statements offer no reason why this should have to be a workaround
-Pthought_sph=interpn(kcyl_for_interp, Pthought_cyl_surv, doubled, bounds_error=False, fill_value=None)
-fig,axs=plt.subplots(1,2,figsize=(12,5))
-Delta2_fidu=kfidu_sph**3*Pfidu_sph/(2*pi**2)
-Delta2_thought=k_sph**3*Pthought_sph/(2*pi**2)
-P=[Pfidu_sph,Pthought_sph]
-Delta2=[Delta2_fidu,Delta2_thought]
-kmodes=[kfidu_sph,k_sph]
-spectra=[P,Delta2]
+suffixes=["systematic-laden and fiducially beamed side-by-side","fractional difference of systematic-laden and fiducially beamed","contaminant beamed, fractional"]
+powers=[["Power\n","Dimensionless power\n"],["",""],["",""]]
+ylabels=["P (K$^2$ Mpc$^3$)","Δ$^2$ (log(K$^2$/K$^2$)"]
 labels=["fiducial","systematic-laden"]
 
-fidu=[Pfidu_sph,Delta2_fidu]
-thought=[Pthought_sph,Delta2_thought]
+# fig,axs=plt.subplots(1,2,figsize=(12,5))
+fig,axs=plt.subplots(3,2,figsize=(12,12))
+for i in range(3):
+    for j in range(2):
+        axs[i,j].set_ylabel(ylabels[j])
+        axs[i,j].set_xlabel("k (1/Mpc)")
+        axs[i,j].set_title(powers[i][j]+suffixes[i])
+for i,epsilon_xy in enumerate(epsilons_xy):
+    epsxy=epsilon_xy
 
-# for i,case in enumerate(P): # index fidu vs. thought
-#     axs[0].loglog(kmodes[i],P[i],label=labels[i]) # probably need to convert the Delta2 to a loglog plot... which defeats the point of i
-#     axs[1].loglog(kmodes[i],Delta2[i],label=labels[i])
+    bundled_gaussian_primary_args=[hpbw_x,hpbw_y]
+    bundled_gaussian_primary_uncs=[epsxy,epsxy]
 
-for i,case in enumerate(fidu):
-    axs[i].loglog(kfidu_sph,case,label="fiducial")
-    axs[i].loglog(k_sph,thought[i],label="frac. unc. in HPBW= "+str(epsxy))
-    axs[i].set_xlabel("k (1/Mpc)")
-    axs[i].set_xlim(k_sph[1],k_sph[-1])
-axs[0].set_ylabel("P (K$^2$ Mpc$^3$)")
-axs[0].set_title("Power")
-axs[1].set_ylabel("Δ$^2$ (log(K$^2$/K$^2$)")
-axs[1].set_title("Dimensionless power")
-axs[1].legend()
-plt.suptitle("900 MHz CHORD-512 survey (high kperp truncated)\nGaussian passband FWHM {:5.2} Mpc; Airy HPBW {:5.3} (x) and {:5.3} (y)\n with respective fractional uncertainties {:4.2}, {:4.2}, and {:4.2}".format(nu_window.sigLoS,hpbw_x,hpbw_y,epsLoS,epsxy,epsxy))
+    nu_window=window_calcs(bminCHORD,bmaxCHORD,
+                            ceil,
+                            "AiryGaussian",bundled_gaussian_primary_args,bundled_gaussian_primary_uncs,
+                            pars_Planck18,pars_Planck18,
+                            n_sph_nu,dpar,
+                            nu_ctr,channel_width,
+                            pars_forecast_names=parnames, no_monopole=False)
+    nu_window.print_survey_characteristics()
+    if redo_window_calc:
+        t0=time.time()
+        nu_window.calc_Pcont_asym()
+        t1=time.time()
+        print("Pcont calculation time was",t1-t0)
+
+        Pcont_cyl_surv=nu_window.Pcont_cyl_surv
+        Ptrue_cyl_surv=nu_window.Ptrue_cyl_surv
+        Pthought_cyl_surv=nu_window.Pthought_cyl_surv
+        Pfidu_sph=nu_window.Ptruesph # self.ksph,self.Ptruesph
+        kfidu_sph=nu_window.ksph
+
+        np.save("Pcont_cyl_surv_"+str(i)+".npy",Pcont_cyl_surv)
+        np.save("Pthought_cyl_surv_"+str(i)+".npy",Pthought_cyl_surv)
+        np.save("Ptrue_cyl_"+str(i)+".npy",Ptrue_cyl_surv)
+        np.save("Pfidu_sph_"+str(i)+".npy",Pfidu_sph)
+        np.save("kfidu_sph_"+str(i)+".npy",kfidu_sph)
+    else:
+        Pcont_cyl_surv=np.load("Pcont_cyl_surv_"+str(i)+".npy")
+        Pthought_cyl_surv=np.load("Pthought_cyl_surv_"+str(i)+".npy")
+        Ptrue_cyl_surv=np.load("Ptrue_cyl_"+str(i)+".npy")
+        Pfidu_sph=np.load("Pfidu_sph_"+str(i)+".npy")
+        kfidu_sph=np.load("kfidu_sph_"+str(i)+".npy")
+
+    N_sph=128
+    kmin_surv=nu_window.kmin_surv
+    kmax_surv=nu_window.kmax_surv
+    k_sph=np.linspace(kmin_surv,kmax_surv,N_sph)
+    kpar=nu_window.kpar_surv
+    kperp=nu_window.kperp_surv
+    k00,k11=np.meshgrid(kpar,kperp,indexing="ij")
+    kcyl_for_interp=(kpar,kperp)
+    Pfidu_sph=np.reshape(Pfidu_sph,(Pfidu_sph.shape[-1],))
+
+    doubled=np.linspace(kmin_surv,kmax_surv,N_sph*2) # print statements offer no reason why this should have to be a workaround
+    Pcont_sph=interpn(kcyl_for_interp, Pcont_cyl_surv, doubled, bounds_error=False, fill_value=None)
+    Pthought_sph=interpn(kcyl_for_interp, Pthought_cyl_surv, doubled, bounds_error=False, fill_value=None)
+    Ptrue_sph=interpn(kcyl_for_interp, Ptrue_cyl_surv, doubled, bounds_error=False, fill_value=None)
+    Delta2_fidu=kfidu_sph**3*Pfidu_sph/(2*pi**2)
+    Delta2_thought=k_sph**3*Pthought_sph/(2*pi**2)
+    Delta2_true=k_sph**3*Ptrue_sph/(2*pi**2)
+    Delta2_cont=k_sph**3*Pcont_sph/(2*pi**2)
+    P=[Pfidu_sph,Pthought_sph]
+    Delta2=[Delta2_fidu,Delta2_thought]
+    kmodes=[kfidu_sph,k_sph]
+    spectra=[P,Delta2]
+
+    fidu=[Pfidu_sph,Delta2_fidu]
+    thought=[Pthought_sph,Delta2_thought]
+    true=[Ptrue_sph,Delta2_true]
+    cont=[Pcont_sph,Delta2_cont]
+
+    for k,case in enumerate(fidu):
+        label_for_eps="frac. unc. in HPBW= "+str(epsxy)
+        axs[0,k].loglog(k_sph,true[k],label="fiducially beamed data",c="C1")
+        axs[0,k].loglog(k_sph,thought[k],label=label_for_eps,c=blues_here[i])
+
+        axs[1,k].plot(k_sph,(true[k]-thought[k])/true[k],label=label_for_eps,c=blues_here[i])
+
+        axs[2,k].plot(k_sph,cont[k]/true[k],label=label_for_eps,c=blues_here[i])
+
+        for m in range(3):
+            axs[m,k].set_xlim(k_sph[1],k_sph[-1])
+    axs[0,1].legend()
+plt.suptitle("900 MHz CHORD-512 survey (high kperp truncated)\nAiry HPBW {:5.3} (x) and {:5.3} (y)".format(hpbw_x,hpbw_y))
 plt.tight_layout()
 plt.savefig("contaminant_power_test.png")
 plt.show()
